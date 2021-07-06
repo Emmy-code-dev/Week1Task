08b3d8ebc4017523e3226206923e6611ea04b518
EmmyRonald <emmyronald@gmail.com>
2021-07-06 12:55:13 +0100

    first commit

---
+++ sumfunction.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+      <meta charset="utf-8">
+      <title></title>
+    </head>
+    <body>
+      <?php
+      $sum=array(5,15,25);
+      echo array_sum($sum);
+      ?>
+
+
+    </body>
+ </html>
