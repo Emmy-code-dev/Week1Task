aeefef6c6ca1f9f53382f7c67fa0be32bb3aedd6
EmmyRonald <emmyronald@gmail.com>
2021-07-06 12:01:01 +0100

    first commit

---
+++ range.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+      <meta charset="utf-8">
+      <title></title>
+    </head>
+    <body>
+      <?php
+      $number = range(0,50,10);
+      print_r ($number);
+      ?>
+
+    </body>
+ </html>
